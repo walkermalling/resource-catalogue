549ccf9a47a0fda7b528e55012d9384c29c7a7c0
Walker Malling <walker.malling@live.com>
2014-08-18 12:56:34 -0700

    update dependencies

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ var server = http.createServer(app);
 app.set('port', process.env.PORT || 3000 );
 
 server.listen(app.get('port'), function() {
-  console.log('Server running on :3000');
+  console.log('Server running');
 });
